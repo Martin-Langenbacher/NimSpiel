7eba3102da23d23df180991a4c9c2fb0322b6b01
Martin Langenbacher <martin.langenbacher@telekom.de>
2020-04-16 00:44:15 +0200

    Zwischenstand

---
+++ src/nimspiel/Teilen.java
@@ -0,0 +1,15 @@
+package nimspiel;
+
+public class Teilen {
+	
+	public int haufenNummer, teilenBeiNummer;
+	
+	public Teilen(int haufenNummer, int teilenBeiNummer) {
+		this.haufenNummer = haufenNummer;
+		this.teilenBeiNummer = teilenBeiNummer;
+	}
+
+}
+
+
+
--- src/nimspiel/nimspiel.java
+++ src/nimspiel/nimspiel.java
@@ -13,6 +13,7 @@ public class nimspiel {
 		
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
+		Scanner scanner = new Scanner(System.in);
 		
 		boolean zahlOk = false;
 		int min = 5;
@@ -21,6 +22,9 @@ public class nimspiel {
 		int nWood;
 		String nameOfPlayer;
 		int numberOfHaufen = 1;
+		boolean gamePlaying = true;
+		boolean isGameOver = false;
+		int zaehler = 0;
 		
 		// What is your name:
 		Scanner myObj = new Scanner(System.in);
@@ -66,80 +70,59 @@ public class nimspiel {
 			numberOfHaufen++;
 			
 			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
+			while (gamePlaying) {
+				boolean moveOk = true;
+				
+				do {
+					if (!moveOk) {
+						System.out.println("Cell already filled !");
+					}
+					while (zaehler == 0) {
+
+						try {
+							System.out.println("Bitte geben Sie die Nummer des Haufens ein und die Anzahl wie Sie den Haufen aufteilen wollen");
+							System.out.println("Format [Haufen] [Hölzer]");
+							Teilen userMove = new Teilen(scanner.nextInt(), scanner.nextInt());
+							System.out.println("Hier: Etwas tun mit der Eingabe: " + userMove);
+						}
+						catch (NumberFormatException ausnahme) {
+							System.out.println("Bitte eine Zahl eingeben!");
+							continue;
+						}
+						zaehler++;
+						}
+					
+					// b.placeAMove(userMove, Board.PLAYER_O);
+					
+				} while (!moveOk);
+				// Achtung: Vorher noch etwas tun mit der Eingabe, sonst geht es schief...!!!
+				zeigeAlleHaufen(haufen, numberOfHaufen);
+				
+				if (isGameOver)
+					break;
+				
+				// Computer move
+				// b.minimax(0, Board.PLAYER_X);
+				//System.out.println("DAs kommt von minimax zurück: " + b.minimax(0, Board.PLAYER_X));
+				System.out.println();
+				System.out.println("Computer choose postion : ");
+				
+				//b.placeAMove(b.computerMove, Board.PLAYER_X);	
+				zeigeAlleHaufen(haufen, numberOfHaufen);
+			}
 			
 			/*
-			
-			
-		ArrayList<Wort> words = new ArrayList<>();
-		
-		if (eingabeString.equals("")) {
-			eingabeString = "Fehler!";
-		}
-		
-		
-		// String in Wörter aufteilen
-		int i = 0;
-		do {
-		  String buchstabe = "" + eingabeString.charAt(i);
-			
-			
-			
-			if (buchstabe.equals(" ")) {
-				if (newWordString != "") {
-					words.add(new Wort(i, newWordString));
-					amountOfWords++;
-					newWordString = "";
-				} else {
-					newWordString = "";
-				}
-				
-			} else {
-				newWordString = newWordString + buchstabe;
-			}
-		  i++;
-		}
-		while (i < eingabeString.length());
-		words.add(new Wort(i, newWordString));
-		amountOfWords++;
-		
-		System.out.println("amount of Words: " + amountOfWords);
-		
-		
-		// Vergleich der Wörter
-		for (int j = 0; j < amountOfWords-1; j++) {
-			for (int m = j+1; m < amountOfWords; m++) {
-				
-				if ((words.get(j).getNumberOfWord() != 0) && Wort.compareWords(words.get(j).getWordString(), words.get(m).getWordString())) {
-					// words are the same
-					words.get(j).setNumberOfWord(words.get(j).getNumberOfWord() + 1);
-					words.get(m).setNumberOfWord(0);
-				} 	
-			}
-		}
-		
-		
-		// ausgeben der Liste...
-		for (int j = 0; j < amountOfWords; j++) {
-			if (words.get(j).getNumberOfWord() != 0 && !words.get(j).getWordString().equals("")) {
-				System.out.println(words.get(j).getWordString() + ": " +words.get(j).getNumberOfWord());
-				
-			}	
-		}
-	}
-
+			if (b.hasPlayerWon(Board.PLAYER_X))
+				System.out.println("You lost !");
+			else if(b.hasPlayerWon(Board.PLAYER_O))
+				System.out.println(" You win !");
+			else
+				System.out.println("Draw !");
+						
 			*/
-
+		
+			
+			
 			
 		}		
 
